fad6ba8cf964c497bc3c4d4b15267d870d92889e
Michaelsun Baluyos <s3110401@b01409015-09.cs.rmit.edu.au>
2015-05-12 15:13:04 +1000

    commit test

---
+++ IPSE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IPSE
+//
+//  Created by Michaelsun Baluyos on 12/05/2015.
+//  Copyright (c) 2015 rmit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
