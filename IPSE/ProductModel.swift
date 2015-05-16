d463c6273bda4d89b3e4cc8e5f40702ac2f27a0e
“michaelsunb” <s3110401@student.rmit.edu.au>
2015-05-16 16:59:19 +1000

    modify ProductModel to use an array of ProductTable and to use singleton

--- IPSE/ProductModel.swift
+++ IPSE/ProductModel.swift
@@ -9,8 +9,9 @@
 import Foundation
 import CoreData
 
-// http://
-// www.jessesquires.com/better-coredata-models-in-swift/
+/**
+ * http://www.jessesquires.com/better-coredata-models-in-swift/
+ */
 class ProductModel : NSManagedObject {
     @NSManaged private var productID : Int
     @NSManaged private var productName : String
@@ -18,22 +19,41 @@ class ProductModel : NSManagedObject {
     @NSManaged private var productCost : Float
     @NSManaged private var productImag : String
     
-    var product: ProductTable {
+    var product: [ProductTable] {
         get {
-            return ProductTable(
+            return [ProductTable(
                 id:self.productID,
                 name:self.productName,
                 description:self.productDesc,
                 cost:self.productCost,
                 product_image:self.productImag
-            )
+            )]
         }
         set {
-            self.productID = newValue.id
-            self.productName = newValue.name
-            self.productDesc = newValue.description
-            self.productCost = newValue.cost
-            self.productImag = newValue.product_image
+            for newVal in newValue {
+                self.productID = newVal.id
+                self.productName = newVal.name
+                self.productDesc = newVal.description
+                self.productCost = newVal.cost
+                self.productImag = newVal.product_image
+            }
         }
+    }
+    
+    /**
+     * Reference to thread safe singleton at bottom on page
+     * http://code.martinrue.com/posts/the-singleton-pattern-in-swift
+     */
+    class var sharedInstance: ProductModel {
+        struct Static {
+            static var instance: ProductModel?
+            static var token: dispatch_once_t = 0
+        }
+        
+        dispatch_once(&Static.token) {
+            Static.instance = ProductModel()
+        }
+        
+        return Static.instance!
     }
 }
