9b3b1d7aafd3a7ef943a6ac28bec8e5f4bcc0045
TonyAltas <tonyaltas@gmail.com>
2015-05-22 21:08:27 -0700

    Update Model

--- IPSE/Model.swift
+++ IPSE/Model.swift
@@ -1,9 +1,39 @@
 //
 //  Model.swift
-//  IPSE
+//  SwiftCallURL
 //
-//  Created by Monster on 22/05/2015.
-//  Copyright (c) 2015 rmit. All rights reserved.
+//  Created by Monster on 21/05/2015.
+//  Copyright (c) 2015 Monster. All rights reserved.
 //
 
 import Foundation
+
+
+class Model {
+    private let dataAccessObject:BlueMixDAO
+    private struct Static
+    {
+        static private var instance:Model?
+    }
+    
+    class var sharedInstance: Model
+    {
+        if !(Static.instance != nil)
+        {
+            Static.instance = Model()
+        }
+        return Static.instance!
+    }
+    
+    private init(){
+        dataAccessObject = BlueMixDAO()
+    }
+    
+    func getString() -> String{
+        return "Testing the model --> GetString Method"
+    }
+    
+    func login() -> Bool{
+        return dataAccessObject.loginRequest()
+    }
+}
